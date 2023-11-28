02841e8f33cda12e922cec4be6c98e8d7a49d2da
Ricardo Diaz <rdiaz153@student.mtsac.edu>
2023-11-27 22:55:24 -0800

    11/27

--- project2/project2.cpp
+++ project2/project2.cpp
@@ -30,7 +30,7 @@ int main()
     cout << "There are 5 problems per session. The computer will select a problem from a list of problems and you have up to three attempts per problem. " << endl;
     cout << "You must provide an answer in reduced form (i.e., enter 3/4 instead of 6/8). Have fun and good luck!\n\n";
 	
-	loadProblems(string problems[], int& problemCount);
+	loadProblems(problems[], problemCount);
     
 	cout << "Available Modes\n";
 	cout << "1. Training mode\n";
