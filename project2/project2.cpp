ba88566ea77d4ba68fbdb89b05021d59e89b526a
rdiaz153 <142759822+rdiaz153@users.noreply.github.com>
2023-11-27 22:00:03 -0800

    11/27

---
+++ project2/project2.cpp
@@ -0,0 +1,39 @@
+/* Program: Project 2 Program
+	Author: Ricardo Diaz
+	Class: CSCI 110
+	Date: 11/28/23
+	Description: Create the program for Project 2.
+	I certify that the code below is my own work.
+	Exception(s): N/A
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+void projectMenu();
+void loadProblems(string problems[], int& count);
+int gcd(int a, int b);
+
+
+int main()
+{
+    projectMenu();
+    
+
+
+
+    return 0;
+}
+
+void projectMenu()
+{
+    cout << "Welcome to the Fraction Tutor V1 by Ricardo Diaz!\n\n"
+    cout << "There are 5 problems per session. The computer will select a problem from a list of problems and you have up to three attempts per problem. 
+    You must provide an answer in reduced form (i.e., enter 3/4 instead of 6/8). Have fun and good luck.\n\n"
+
+    
+}
