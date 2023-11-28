d91ae5f0342850615d73c23a11e150b4d1de4f21
Ricardo Diaz <rdiaz153@student.mtsac.edu>
2023-11-27 22:54:26 -0800

    11/27

--- project2/project2.cpp
+++ project2/project2.cpp
@@ -14,26 +14,58 @@
 
 using namespace std;
 
-void projectMenu();
-// void loadProblems(string problems[], int& count);
-// int gcd(int a, int b);
+void loadProblems(string problems[], int& count);
+int gcd(int a, int b);
 
+const int MAX_NUMS = 100;
 
 int main()
 {
-    projectMenu();
+	srand(static_cast<unsigned int>(time(0)));
+
+	string problems[MAX_NUMS];
+	int problemCount, mode;
+
+    cout << "Welcome to the Fraction Tutor V1 by Ricardo Diaz!\n\n";
+    cout << "There are 5 problems per session. The computer will select a problem from a list of problems and you have up to three attempts per problem. " << endl;
+    cout << "You must provide an answer in reduced form (i.e., enter 3/4 instead of 6/8). Have fun and good luck!\n\n";
+	
+	loadProblems(string problems[], int& problemCount);
     
+	cout << "Available Modes\n";
+	cout << "1. Training mode\n";
+	cout << "2. Normal mode\n\n";
+	cout << "Please select a mode: ";
+	cin >> mode;
 
+	while (mode < 1 && mode > 2)
+	{
+		cout << "That is an invalid mode option. Please correctly enter which mode you would like to use." << endl;
+		cin >> mode;
 
+	}
 
     return 0;
 }
 
-void projectMenu()
+void loadProblems(string problems[], int& count)
 {
-    cout << "Welcome to the Fraction Tutor V1 by Ricardo Diaz!\n\n";
-    cout << "There are 5 problems per session. The computer will select a problem from a list of problems and you have up to three attempts per problem. " << endl;
-    cout << "You must provide an answer in reduced form (i.e., enter 3/4 instead of 6/8). Have fun and good luck!\n\n";
 
-    
+ifstream FracFile("P2Fractions.txt");
+count = 0;
+
+if (!FracFile)
+{
+	cout << "Error: Unable to open P2Fractions.txt" << endl;
+	return 0;
+
+}
+
+while (getline(FracFile, problems[count]))
+{
+	count++;
+}
+
+FracFile.close();
+
 }
